2a95ed837d612894dbb5ec9024ed2417608f3dd1
SumeetKul <SumeetKul@users.noreply.github.com>
2022-09-04 17:52:21 -0500

    Add files via upload

---
+++ fpaywall.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import time
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("link",nargs=1)
+args = parser.parse_args()
+
+print(args.link[0])
+
+options = Options()
+
+prefs = {"profile.managed_default_content_settings.javascript" : 2}
+
+# add prefs 
+options.add_experimental_option("prefs", prefs)
+options.add_argument("--disable-javascript")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+driver.get(args.link[0])
+time.sleep(100)
+#driver.refresh()
+#driver = webdriver.Chrome(chrome_options=options)
