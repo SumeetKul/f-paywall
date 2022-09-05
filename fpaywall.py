b6b0ecd5242e63e7ae19ed8fee21116bfd5b1fea
SumeetKul <SumeetKul@users.noreply.github.com>
2022-09-04 21:45:41 -0500

    Update fpaywall.py

--- fpaywall.py
+++ fpaywall.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.options import Options
 import time
 from argparse import ArgumentParser
 
+# Get link from the command line argument
 parser = ArgumentParser()
 parser.add_argument("link",nargs=1)
 args = parser.parse_args()
@@ -20,8 +21,9 @@ prefs = {"profile.managed_default_content_settings.javascript" : 2}
 # add prefs 
 options.add_experimental_option("prefs", prefs)
 options.add_argument("--disable-javascript")
+
+# setup driver and keep running for an hour
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
 driver.get(args.link[0])
-time.sleep(100)
-#driver.refresh()
-#driver = webdriver.Chrome(chrome_options=options)
+time.sleep(3600)
+
